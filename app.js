164c5dae35f88a0d2df815e231bd136829c572c3
Eder Marques <eder.marques@citi.org.br>
2018-08-29 21:25:21 -0300

    now checks multiple line comments and files with more comments than code

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ fs.readFile(process.argv[2], { encoding: 'utf-8' }, (err, data) => { // utf-8, t
   if (process.argv[2].split('.')[1] === 'py') commentSign = '#'; // comments in python
   let totalLines = 0;
   let comments = 0;
+  let MLCFlag = false; // MLC stands for Multiple Line Comment
   data.split('\n').forEach((line) => { // get the number of lines of the file
     totalLines += 1;
     if (line.includes(commentSign)) comments += 1; // count only one comment per line
+    if (line.includes('/*')) MLCFlag = true; // turn on the flag to count every line of the comment
+    if (line.includes('*/')) {
+      // end of the comment, don't count anymore
+      comments += 1;
+      MLCFlag = false;
+     } 
+    if (MLCFlag) comments += 1;
   });
 
   const statusArray = ['terrible', 'bad', 'regular', 'good', 'excelent', 'excelent', 'maybe too much...']; // feedback to the user
@@ -16,7 +24,7 @@ fs.readFile(process.argv[2], { encoding: 'utf-8' }, (err, data) => { // utf-8, t
   const ratio = Math.floor(comments / totalLines * 100); // percentage of comments in the file
 
   const index = Math.floor(ratio / 8); // round it down to get the right status
-  const status = statusArray[index].toUpperCase();
+  const status = index < 6 ? statusArray[index].toUpperCase() : 'There\'s more comments than code...';
 
   const fileName = process.argv[2].split('/')[process.argv[2].split('/').length - 1];
   
